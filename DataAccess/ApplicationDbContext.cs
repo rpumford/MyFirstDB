afb2a235012b6e8b26b5b26bf110e13fa7c2bbed
Ryan Pumford <rpumford@gmail.com>
2021-06-09 03:44:32 -0400

    Add project files.

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyFirstDB.Models;
+
+namespace MyFirstDB.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Hops> Hops { get; set; }
+    }
+}
---
+++ Models/Beer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstDB.Models
+{
+    public class Beer
+    {
+        [Key]
+        public string name { get; set; }
+        public string style { get; set; }
+        public List<Hops> hops { get; set; }
+        public decimal rating { get; set; }
+    }
+
+    public class Hops
+    {
+        [Key]
+        public string name { get; set; }
+        public string flavor { get; set; }
+    }
+}
